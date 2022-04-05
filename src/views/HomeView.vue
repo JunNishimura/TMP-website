11e1ba0b2ea104728fff7f628f9b9d54627171fc
Jun Nishimura <n.junjun0303@gmail.com>
2022-04-05 18:54:49 +0900

    add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home-container">
+      <div class="home-container__title">
+        TMP(つくばミュージックプロジェクト)
+      </div>
+      <div class="home-container__sentence">
+        筑波大学学祭に毎年有志でバンドを呼んでいる学生団体です！
+      </div>
+      <div class="home-container__sns">
+        <a href="https://twitter.com/tsukubamusicp" class="home-container__sns-twitter">
+          <img src="@/assets/twitter.png">
+        </a>
+        <a href="https://www.instagram.com/tsukuba_music_p/" class="home-container__sns-insta">
+          <img src="@/assets/insta.png">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.home {
+  color: #fff;
+  background-color: #000;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
 }
-</script>
+.home-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: left;
+}
+.home-container a {
+  cursor: pointer;
+}
+.home-container img {
+  width: 24px;
+}
+.home-container__title {
+  font-size: 22px;
+}
+.home-container__sentence {
+  margin-top: 32px;
+}
+.home-container__sns {
+  margin-top: 18px;
+  display: flex;
+  justify-content: flex-start;
+}
+.home-container__sns-twitter, .home-container__sns-insta {
+  margin-right: 16px;
+}
+</style>
